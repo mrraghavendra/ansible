0adee1d8dd84e46bf1f69a56dca7c27ca536093f
mrraghavendra <mrraghavendra@gmail.com>
2018-09-19 21:42:39 +0530

    changed
    changed

--- filter_plugins/my_filters.py
+++ filter_plugins/my_filters.py
@@ -211,7 +211,7 @@ class FilterModule(object):
         template=j2_env.get_template('rulestemplate.json')
         segmenttemplate=j2_env.get_template('SegmenQOStemplate.json')
         insertqostemplate=j2_env.get_template('insert_QOS.json')
-        print(a_variable)
+        print("test:"+a_variable)
         i=0
         obj_json = json.loads(a_variable)
         ruletem =""
