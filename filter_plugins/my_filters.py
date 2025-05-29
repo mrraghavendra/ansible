d22060f18a981b0efdea9fa65d4dc7614f802db8
mrraghavendra <37608050+mrraghavendra@users.noreply.github.com>
2025-05-29 17:54:20 +0530

    Update my_filters.py

--- filter_plugins/my_filters.py
+++ filter_plugins/my_filters.py
@@ -19,19 +19,18 @@ velocloud.configuration.verify_ssl = False
 
 class FilterModule(object):
 
-    
-    # Copilot start
-    def test123(self):
-        print("updated module")
-      }
-     # Copilot end
-
     def filters(self):
         return {'updateDeviceSettings': self.updateDeviceSettings,
                 'updateQOS': self.updateQOS,
                 'json_formatUtil': self.json_formatUtil,
                 'segRulesfilter': self.segRulesfilter
                }
+    # Copilot start
+    def test(self):
+      print("Hello world")
+               }
+    # Copilot end
+    
     # Copilot suggestion
     def updateDeviceSettings(self, a_variable, b_variable):
         velocloud.configuration.verify_ssl = False
