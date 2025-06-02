1ceba4f385e22fc6c4c30fa36a289d318392bc68
mrraghavendra <37608050+mrraghavendra@users.noreply.github.com>
2025-06-02 19:53:05 +0530

    Update Update_Jira.py

--- scripts/Update_Jira.py
+++ scripts/Update_Jira.py
@@ -1,7 +1,7 @@
 import os
 
-LOC = int(os.getenv("LOC", "0"))
+LOC = int(os.getenv("LINE_OF_CODE", "0"))
 print(f"Line Of Code: {LOC}")
 
-CopilotLOC = int(os.getenv("CopilotLOC", "0"))
+CopilotLOC = int(os.getenv("Copilot_LINE_OF_CODE", "0"))
 print(f"Copilot Line Of Code: {CopilotLOC}")
