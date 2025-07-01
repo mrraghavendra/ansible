213e132c8c5387dc04967177b32e824b1fb2aa32
mrraghavendra <37608050+mrraghavendra@users.noreply.github.com>
2025-07-01 09:31:18 +0530

    Update Update_Jira.py

--- scripts/Update_Jira.py
+++ scripts/Update_Jira.py
@@ -7,7 +7,7 @@ print(f"Line Of Code: {LOC}")
 CopilotLOC = int(os.getenv("COPILOT_LINES_OF_CODE", "0"))
 print(f"Copilot Line Of Code: {CopilotLOC}")
 
-result = subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True, text=True)
+result = subprocess.run(["git", "symbolic-ref", "--short", "HEAD"], capture_output=True, text=True)
 branch_name = result.stdout.strip()
-print(f"Current branch: {result.stdout}")
-print(f"Current branch: {branch_name}")
+
+print(f"Current branch 1: {branch_name}")
